e1321ef49081272481afc3bcc7258800002cddb6
unknown <jacoblinleymoore@gmail.com>
2025-03-11 16:02:20 -0500

    Adding OpenMP examples

---
+++ examples/OpenMP/Example2/solutions/vecadd_opt3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <omp.h>
+#include "timer.h"
+
+// large enough to force into main memory
+#define ARRAY_SIZE 80000000
+static double a[ARRAY_SIZE], b[ARRAY_SIZE], c[ARRAY_SIZE];
+
+void vector_add(double *c, double *a, double *b, int n);
+
+int main(int argc, char *argv[]){
+   #pragma omp parallel
+      if (omp_get_thread_num() == 0)
+         printf("Running with %d thread(s)\n",omp_get_num_threads());
+
+   struct timespec tstart;
+   double time_sum = 0.0;
+#pragma omp parallel
+   {
+#pragma omp for
+      for (int i=0; i<ARRAY_SIZE; i++) {
+         a[i] = 1.0;
+         b[i] = 2.0;
+      }
+
+#pragma omp master
+      cpu_timer_start(&tstart);
+   vector_add(c, a, b, ARRAY_SIZE);
+#pragma omp master
+      time_sum += cpu_timer_stop(tstart);
+   } // end of omp parallel
+
+   printf("Runtime is %lf msecs\n", time_sum);
+}
+
+void vector_add(double *c, double *a, double *b, int n)
+{
+#pragma omp for
+   for (int i=0; i < n; i++){
+      c[i] = a[i] + b[i];
+   }
+}
