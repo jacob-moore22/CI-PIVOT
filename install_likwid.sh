549ab0daa3a58ed6309b15c4360a9c3d0d364305
unknown <jacoblinleymoore@gmail.com>
2025-03-20 11:52:56 -0500

    ENH: updating install likwid script

--- install_likwid.sh
+++ install_likwid.sh
@@ -3,18 +3,18 @@
 # Exit on error
 # set -e
 
-# echo "Installing LIKWID prerequisites..."
-# # Check if running as root/sudo
-# if [ "$EUID" -ne 0 ]; then
-#     echo "Please run as root or with sudo"
-#     exit 1
-# fi
+echo "Installing LIKWID prerequisites..."
+# Check if running as root/sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root or with sudo"
+    exit 1
+fi
 
-# # Install prerequisites (for Debian/Ubuntu systems)
-# if command -v apt-get >/dev/null; then
-#     apt-get update
-#     apt-get install -y build-essential gcc make git
-# fi
+# Install prerequisites (for Debian/Ubuntu systems)
+if command -v apt-get >/dev/null; then
+    apt-get update
+    apt-get install -y build-essential gcc make git
+fi
 
 # Create a directory for installation
 INSTALL_DIR="/tmp/likwid_install"
@@ -28,7 +28,7 @@ cd likwid
 echo "Configuring LIKWID..."
 # Modify config.mk to set installation prefix to /usr/local
 sed -i 's/^PREFIX ?= \/usr\/local/PREFIX ?= \/usr\/local/' config.mk
-# # Enable access daemon
+# Enable access daemon
 sed -i 's/^ACCESSMODE ?= direct/ACCESSMODE ?= accessdaemon/' config.mk
 
 echo "Building LIKWID..."
@@ -37,8 +37,8 @@ make
 echo "Installing LIKWID..."
 make install
 
-echo "Cleaning up..."
-cd ../..
-rm -rf $INSTALL_DIR
+# echo "Cleaning up..."
+# cd ../..
+# rm -rf $INSTALL_DIR
 
 echo "LIKWID installation completed!"
